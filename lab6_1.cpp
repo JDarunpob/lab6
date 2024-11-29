4fe3a87c5a0a0867ba00fd479dc087b3a0ba3f49
JDarunpob <darunpob.jean@gmail.com>
2024-11-29 16:18:57 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
+    int i;
+    int x[2]={0,0};
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    cin>>i;
+    while(i!=0){
+        if(i%2==0){
+            x[0]++;
+        }else{
+            x[1]++;
+        }
+        cout << "Enter an integer: ";
+        cin>>i;
+    }
+    cout << "#Even numbers = "<< x[0]<<"\n";
+    cout << "#Odd numbers = "<< x[1]<<"\n";
 }
